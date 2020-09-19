d4abfe8eb897529081d88eeb3b7e4dd505adf969
yusard <y.ardavardar@gmail.com>
2020-09-19 18:42:46 +0300

    server ve veriler oluşturuldu

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 8000;
+const data = require("./data.json");
+let port = process.env.PORT || 8000;
 
 app.get("/",(req,res)=>{
-    res.send("hh");
+    res.send(data)
 });
 
-app.listen(port);
+app.get("/random",(req,res)=>{
+    var random = data[Math.floor(Math.random() * data.length)];
+    console.log(random)
+    res.send(random);
+});
+
+app.listen(port, ()=>{
+    console.log("port başladı")
+});
